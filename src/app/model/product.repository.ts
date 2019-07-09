ace78acfe67b7cb23840fa0c9ed7b1d62dc41f86
Giorgos Giourmetakis <giorgosggg@gmail.com>
2019-07-09 14:30:17 +0300

    changes

--- src/app/model/product.repository.ts
+++ src/app/model/product.repository.ts
@@ -18,23 +18,16 @@ export class ProductRepository {
         new Product(8, "Η Κόμη της Βερενίκης", "Επιστήμες-Τεχνολογία", "ΓΙΩΡΓΟΣ ΓΡΑΜΜΑΤΙΚΑΚΗΣ", "assets/komi-tis-verenikis.jpg", 20),
         new Product(9, "David Bowie – Αναζητώντας τον Starman", "Μουσική", "ΜΑΡΙΑ ΜΑΡΚΟΥΛΗ", "assets/starman.jpg", 18),
         new Product(10, "HTML and CSS", "Πληροφορική-Υπολογιστές", "JON DUCKETT", "assets/html.jpg", 11),
-        new Product(11, "Οι φιλόσοφοι του αρχαίου κόσμου", "Φιλοσοφία", "TREVOR CURNOW", "img/bowie.jpg", 21),
+        new Product(11, "Οι φιλόσοφοι του αρχαίου κόσμου", "Φιλοσοφία", "TREVOR CURNOW", "assets/filosofoi.jpg", 21),
         new Product(12, "Ο Βίος και η Πολιτεία του Σκρούτζ Μακ Ντακ", "Κόμικς", "ΝΤΟΝ ΡΟΣΑ", "assets/vios-kai-politeia.jpg", 8),
-        new Product(13, "Product 13", "Ιστορία", "Α' Παγκόσμιος Πόλεμος", "img/bowie.jpg", 16),
-        new Product(14, "Product 14", "Κινηματογράφος", "Στάνλεϊ Κιούμπρικ", "img/bowie.jpg", 25),
+        new Product(13, "Μια σταγόνα ιστορία", "Ιστορία", "ΔΗΜΗΤΡΗΣ ΚΑΜΠΟΥΡΑΚΗΣ", "assets/mia-stagona-istoria.jpg", 16),
+        new Product(14, "The Stanley Kubrick Archives", "Κινηματογράφος", "ALISON CASTLE", "assets/kubrick.jpg", 25),
         new Product(15, "Βασική γραφιστική υποδομή", "Γραφιστική-Design", "ΣΠΥΡΟΣ ΜΠΕΣΗΣ", "assets/grafistiki.jpg", 18),
-        new Product(16, "Έγκλημα και Τιμωρία", "Λογοτεχνία", "ΝΤΟΣΤΟΓΙΕΦΣΚΙ ΦΙΟΝΤΟΡ", "assets/eglima.jpg", 15),
-        new Product(17, "Rock Εγκυκλοπαίδεια", "Μουσική", " ΣΥΛΛΟΓΙΚΟ ΕΡΓΟ", "assets/rock.jpg", 22),
-        new Product(18, "100 Cult Films", "Κινηματογράφος", "ERNEST MATHIJS, XAVIER MENDIK", "assets/cult-films.jpg", 20.90),
-        new Product(19, "Έγκλημα και Τιμωρία", "Λογοτεχνία", "ΝΤΟΣΤΟΓΙΕΦΣΚΙ ΦΙΟΝΤΟΡ", "assets/eglima.jpg", 15),
-        new Product(20, "Rock Εγκυκλοπαίδεια", "Μουσική", " ΣΥΛΛΟΓΙΚΟ ΕΡΓΟ", "assets/rock.jpg", 22),
-        new Product(21, "100 Cult Films", "Κινηματογράφος", "ERNEST MATHIJS, XAVIER MENDIK", "assets/cult-films.jpg", 20.90),
-        new Product(22, "Έγκλημα και Τιμωρία", "Λογοτεχνία", "ΝΤΟΣΤΟΓΙΕΦΣΚΙ ΦΙΟΝΤΟΡ", "assets/eglima.jpg", 15),
-        new Product(23, "Rock Εγκυκλοπαίδεια", "Μουσική", " ΣΥΛΛΟΓΙΚΟ ΕΡΓΟ", "assets/rock.jpg", 22),
-        new Product(24, "100 Cult Films", "Κινηματογράφος", "ERNEST MATHIJS, XAVIER MENDIK", "assets/cult-films.jpg", 20.90),
-        new Product(25, "Έγκλημα και Τιμωρία", "Λογοτεχνία", "ΝΤΟΣΤΟΓΙΕΦΣΚΙ ΦΙΟΝΤΟΡ", "assets/eglima.jpg", 15),
-        new Product(26, "Rock Εγκυκλοπαίδεια", "Μουσική", " ΣΥΛΛΟΓΙΚΟ ΕΡΓΟ", "assets/rock.jpg", 22),
-        new Product(27, "100 Cult Films", "Κινηματογράφος", "ERNEST MATHIJS, XAVIER MENDIK", "assets/cult-films.jpg", 20.90)
+        new Product(16, "Elements of graphic design", "Γραφιστική-Design", "RICK JOHNSON", "assets/elements.jpg", 15),
+        new Product(17, "Σολάρις", "Λογοτεχνία", "ΣΤΑΝΙΣΛΑΒ ΛΕΜ", "assets/solaris.jpg", 15),
+        new Product(18, "1001 Albums You Must Hear Before You Die", "Μουσική", "ROBERT DIMERY", "assets/1001-albums.jpg", 22),
+        new Product(19, "Κυνηγώντας το μεγάλο ψάρι", "Κινηματογράφος", "DAVID LYNCH", "assets/megalo-psari.jpg", 22),
+        new Product(20, "Τα ελληνικά Comics", "Κόμικς", "SOLOUP", "assets/comics.jpg", 20.90)
     ];
 
     constructor(private dataSource: StaticDataSource) {
