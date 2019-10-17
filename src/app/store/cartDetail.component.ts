2f49036838514add77b302d62fbae88a0a7a22b9
Giorgos Giourmetakis <giorgosggg@gmail.com>
2019-10-17 12:26:38 +0300

    responsive books images

--- src/app/store/cartDetail.component.ts
+++ src/app/store/cartDetail.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 import { Cart } from "../model/cart.model";
 
 @Component({
-    templateUrl: "cartDetail.component.html"
+    templateUrl: "cartDetail.component.html",
+    styleUrls: ["cartDetail.component.css"]
 })
 export class CartDetailComponent {
 
